eb9fb88faf38ae4ac4541e2836395b910b69fea5
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-17 17:26:54 -0300

    add configuracao do banco sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TarefaModule } from './tarefa/tarefa.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './ormconfig';
+
 
 @Module({
-  imports: [TarefaModule],
+  imports: [TarefaModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/ormconfig.ts
@@ -0,0 +1,10 @@
+// configuracao do banco SQLITE
+import { DataSourceOptions } from 'typeorm';
+import { Tarefa } from './tarefa/entities/tarefa.entity';
+
+export const config: DataSourceOptions = {
+    type: 'sqlite',
+    database: ':memory:',
+    entities: [Tarefa],
+    synchronize: true,
+  };
