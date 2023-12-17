82609e9bb2477a3d7174c2064a75c3870138ac30
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-17 17:28:34 -0300

    add modelagem da tabela tarefa

--- src/tarefa/entities/tarefa.entity.ts
+++ src/tarefa/entities/tarefa.entity.ts
@@ -1,1 +1,19 @@
-export class Tarefa {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+
+@Entity({ name: 'tarefa' })
+export class Tarefa {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ length: 100, nullable: false })
+  titulo: string;
+
+  @Column({ length: 500, nullable: false })
+  descricao: string;
+
+  @Column({default: false}) //inicia como false pois a tarefa não foi concluida
+  status: boolean;
+}
+  
+
