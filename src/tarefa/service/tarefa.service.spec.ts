a94f04e124ab5e17d5a5ce6dc0f0261c8d655c39
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-24 19:16:02 -0300

    adicionando testes da Tarefa service

--- src/tarefa/service/tarefa.service.spec.ts
+++ src/tarefa/service/tarefa.service.spec.ts
@@ -1,18 +1,205 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TarefaService } from './tarefa.service';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Tarefa } from '../entities/tarefa-entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
+const tarefaMock = new Tarefa({ id: 1, titulo: 'Livro', descricao: 'Ler o livro da faculdade', status: true });
+
+const tarefaListMock: Tarefa[] = [
+  new Tarefa({ id: 1, titulo: 'Ler livro', descricao: 'ler o livro da faculdade', status: false })
+]
+
+const tarefaUpdatedMock = new Tarefa({ id: 1, titulo: 'Ler livro', descricao: 'Terminar de ler o livro da faculdade', status: true });
+
+const tarefaDeletedMock = {
+  "raw": [],
+  "affected": 1
+}
 
 describe('TarefaService', () => {
+
   let service: TarefaService;
+  let repository: Repository<Tarefa>;
 
   beforeEach(async () => {
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TarefaService],
+      providers: [
+        TarefaService,
+        {
+          provide: getRepositoryToken(Tarefa),
+          useValue: {
+            save: jest.fn().mockResolvedValue(tarefaMock),
+            find: jest.fn().mockResolvedValue(tarefaListMock),
+            findOne: jest.fn().mockResolvedValue(tarefaUpdatedMock),
+            merge: jest.fn().mockResolvedValue(tarefaUpdatedMock),
+            delete: jest.fn().mockResolvedValue(tarefaDeletedMock),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<TarefaService>(TarefaService);
+    repository = module.get<Repository<any>>(getRepositoryToken(Tarefa));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(repository).toBeDefined();
   });
+
+  describe('create', () => {
+    it('Deve salvar uma nova tarefa', async () => {
+      // preparacao
+      const tarefaDto = {
+        'titulo': 'Ler livro',
+        'descricao': 'Ler livro da faculdade'
+      }
+
+      // acao
+      const result = await service.create(tarefaDto);
+
+      // validacao
+      expect(result).toEqual(tarefaMock);
+    });
+
+    it('Deve retornar uma excessao', () => {
+      // preparacao
+      jest.spyOn(repository, 'save').mockRejectedValueOnce(new BadRequestException())
+      const tarefaDto = {
+        'titulo': 'Ler livro',
+        'descricao': 'Ler livro da faculdade'
+      }
+
+      // acao
+      const result = service.create(tarefaDto);
+
+      // validacao
+      expect(result).rejects.toThrow(BadRequestException);
+    })
+  });
+
+  describe('findAll', () => {
+    it('Deve retornar uma lista de tarefas', async () => {
+      // acao 
+      const result = await service.findAll();
+
+      // validacao
+      expect(result).toEqual(tarefaListMock);
+    });
+
+    it('Deve lancar uma excecao', () => {
+      // preparacao
+      jest.spyOn(repository, 'find').mockRejectedValueOnce(new BadRequestException())
+
+      //acao
+      const result = service.findAll();
+
+      // validacao
+      expect(result).rejects.toThrow(BadRequestException);
+    });
+
+  });
+
+  describe('update', () => {
+    it('Deve atualizar a tarefa de id 1 com sucesso', async () => {
+      // preparacao 
+      jest.spyOn(repository, 'save').mockResolvedValueOnce(tarefaUpdatedMock);
+      const id = 1;
+      const tarefaUpdateDto = {
+        'status': true
+      }
+
+      // acao
+      const result = await service.update(id, tarefaUpdateDto);
+
+      // validacao
+      expect(result).toEqual(tarefaUpdatedMock);
+
+    });
+
+    it('Deve retornar uma excecao do tipo not found', () => {
+      // preparacao
+      jest.spyOn(repository, 'findOne').mockRejectedValueOnce(new NotFoundException());
+      const id = 3;
+      const tarefaUpdateDto = {
+        'status': true
+      }
+
+      // acao
+      const result = service.update(id, tarefaUpdateDto);
+
+      // validacao
+      expect(result).rejects.toThrow(NotFoundException);
+    });
+
+    it('Deve retornar uma excecao do tipo bad request', () => {
+      // preparacao
+      jest.spyOn(repository, 'save').mockRejectedValueOnce(new BadRequestException());
+      const id = 1;
+      const tarefaUpdateDto = {
+        'status': true
+      }
+
+      // acao
+      const result = service.update(id, tarefaUpdateDto);
+
+      // validacao
+      expect(result).rejects.toThrow(BadRequestException);
+    });
+  });
+
+
+  describe('findOne', () => {
+    it('Deve retornar a tarefa de id 1', async () => {
+      // preparacao
+      const id = 1;
+
+      // acao
+      const result = await service.findOne(id);
+
+      // validacao
+      expect(result).toEqual(tarefaUpdatedMock);
+    })
+
+    it('Deve retornar uma excecao do tipo bad request', () => {
+      // preparacao
+      jest.spyOn(repository, 'findOne').mockRejectedValueOnce(new NotFoundException());
+      const id = 1;
+      //acao
+      const result = service.findOne(id);
+
+      //validacao
+      expect(result).rejects.toThrow(NotFoundException);
+
+    });
+  })
+
+  describe('remove', () => {
+    it('Deve remover a tarefa de id 1 com sucesso', async () => {
+      // preparacao
+      const id = 1;
+
+      // acao
+      const result = await service.remove(id);
+
+      // validacao
+      expect(result).toEqual(tarefaDeletedMock);
+    });
+
+    it('DEve retornar uma excecao do tipo bad request', () => {
+      // preparacao
+      jest.spyOn(repository, 'delete').mockRejectedValueOnce(new BadRequestException());
+      const id = 1;
+
+      // acao
+      const result = service.remove(id);
+
+      // validacao
+      expect(result).rejects.toThrow(BadRequestException);
+    });
+  });
+
 });
--- src/tarefa/service/tarefa.service.ts
+++ src/tarefa/service/tarefa.service.ts
@@ -8,107 +8,116 @@ import { validate } from 'class-validator';
 
 @Injectable()
 export class TarefaService {
- 
+
   constructor(
     @InjectRepository(Tarefa)
-    private tarefaRepository : Repository<Tarefa>
-  ){}
+    private tarefaRepository: Repository<Tarefa>
+  ) { }
 
-   async create(createTarefaDto: CreateTarefaDto): Promise<CreateTarefaDto> {
+  async create(createTarefaDto: CreateTarefaDto): Promise<CreateTarefaDto> {
     const errors = await validate(createTarefaDto);
 
     if (errors.length > 0) {
       throw new BadRequestException('Erro de validação: ' + this.formataErrosDeValidacao(errors));
     }
 
-    try{
+    try {
 
-      return await this.tarefaRepository.save(createTarefaDto) ;
-      
-    }catch(error){
+      return await this.tarefaRepository.save(createTarefaDto);
+
+    } catch (error) {
       throw new BadRequestException('Não foi possivel criar uma nova tarefa')
     }
   }
 
   async findAll(): Promise<CreateTarefaDto[]> {
 
-    try{
+    try {
       let tarefasEncontradas = await this.tarefaRepository.find();
 
       return tarefasEncontradas;
 
-    }catch(error){
+    } catch (error) {
       throw new BadRequestException('Não foi possivel obter a lista de tarefas')
     }
   }
 
-  async  findOne(id: number) : Promise<CreateTarefaDto> {
-    
-    try{
+  async findOne(id: number): Promise<CreateTarefaDto> {
+
+    try {
+
       let tarefaEncontrada = await this.tarefaRepository.findOne({
-        where:{
+        where: {
           id
         }
-      })
-      if(!tarefaEncontrada) {
+      });
+
+      if (!tarefaEncontrada) {
         throw new NotFoundException(`Tarefa ${id} não encontrada!`);
       }
-  
+
       return tarefaEncontrada;
 
-    }catch(error){
-      throw new BadRequestException('Não foi possivel encontrar a tarefa')
+    } catch (error) {
+      throw new NotFoundException('Não foi possivel encontrar a tarefa');
     }
-    
+
   }
-   
 
-  async update(id: number, updateTarefaDto: UpdateTarefaDto) : Promise<CreateTarefaDto>{
+  async update(id: number, updateTarefaDto: UpdateTarefaDto): Promise<CreateTarefaDto> {
     const errors = await validate(updateTarefaDto);
-
+    let tarefaEncontrada;
     if (errors.length > 0) {
       throw new BadRequestException('Erro de validação: ' + this.formataErrosDeValidacao(errors));
     }
-    
-    try{
 
-      let tarefaEncontrada = await this.tarefaRepository.findOne({
-        where:{
+    try {
+
+      tarefaEncontrada = await this.tarefaRepository.findOne({
+        where: {
           id
         }
       })
-  
-      if(!tarefaEncontrada) {
-        throw new NotFoundException(`Tarefa ${id} não encontrada!`);
-      }
+    }
+    catch (error) {
+      throw new NotFoundException(`Tarefa ${id} não encontrada!`);
+
+    }
+
+    try {
+
       this.tarefaRepository.merge(tarefaEncontrada, updateTarefaDto);
       return this.tarefaRepository.save(tarefaEncontrada);
-    }catch(error){
+
+    } catch (error) {
+      console.log('erro ao atualizar: ', error)
       throw new BadRequestException('Não foi possivel atualizar a tarefa')
     }
-    
+
   }
-   
 
   async remove(id: number) {
-    try{
+    try {
       let tarefaEncontrada = await this.tarefaRepository.findOne({
-        where:{
+        where: {
           id
         }
       })
-      if(!tarefaEncontrada) {
+      if (!tarefaEncontrada) {
         throw new NotFoundException(`Tarefa ${id} não encontrada!`);
       }
-      
+
       return await this.tarefaRepository.delete(id);
-    }catch(error){
+    } catch (error) {
       throw new BadRequestException('Não foi possivel deletar a tarefa')
     }
-    
+
   }
 
   private formataErrosDeValidacao(errors: any[]): string {
     return errors.map(error => Object.values(error.constraints).join(', ')).join('; ');
   }
 }
+
+
+
